e1fb68ad65349cee88346277865d54b71cb67313
Zaki Ahmed <ZakiAhmed@alumni.usc.edu>
2021-10-29 01:46:24 -0400

    more pushes

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
+/*
+    SETUP
+*/
+// Express
+// var express = require('express');   // We are using the express library for the web server
+// var app     = express();            // We need to instantiate an express object to interact with the server in our code
+// PORT        = 10026;                 // Set a port number at the top so it's easy to change in the future
+
 let http = require('http');
 let fs = require('fs');
 
@@ -17,3 +25,10 @@ let handleRequest = (request, response) => {
 };
 
 http.createServer(handleRequest).listen(8000);
+
+/*
+    LISTENER
+*/
+// app.listen(PORT, function(){            // This is the basic syntax for what is called the 'listener' which receives incoming requests on the specified PORT.
+//     console.log('Express started on http://localhost:' + PORT + '; press Ctrl-C to terminate.')
+// });
