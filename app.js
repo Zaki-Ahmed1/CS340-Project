743e37f4c60e0b9099da5f077de7d23b1283b7ab
Zaki Ahmed <ahmedza@flip1.engr.oregonstate.edu>
2021-11-01 23:50:46 -0700

    submitted draft

--- app.js
+++ app.js
@@ -2,33 +2,53 @@
     SETUP
 */
 // Express
-// var express = require('express');   // We are using the express library for the web server
-// var app     = express();            // We need to instantiate an express object to interact with the server in our code
-// PORT        = 10026;                 // Set a port number at the top so it's easy to change in the future
+var express = require('express');   // We are using the express library for the web server
+var app     = express();            // We need to instantiate an express object to interact with the server in our code
+PORT        = 12626;                 // Set a port number at the top so it's easy to change in the future
 
 let http = require('http');
 let fs = require('fs');
 
-let handleRequest = (request, response) => {
-    response.writeHead(200, {
-        'Content-Type': 'text/html'
-    });
-    fs.readFile('./index.html', null, function (error, data) {
-        if (error) {
-            response.writeHead(404);
-            respone.write('Whoops! File not found!');
-        } else {
-            response.write(data);
-        }
-        response.end();
-    });
-};
+// let handleRequest = (request, response) => {
+//     response.writeHead(200, {
+//         'Content-Type': 'text/html'
+//     });
+//     fs.readFile('./index.html', null, function (error, data) {
+//         if (error) {
+//             response.writeHead(404);
+//             respone.write('Whoops! File not found!');
+//         } else {
+//             response.write(data);
+//         }
+//         response.end();
+//     });
+// };
 
-http.createServer(handleRequest).listen(8000);
+
+/*
+    ROUTES
+*/
+app.get('/', function(request, response)
+    {
+        response.writeHead(200, {
+            'Content-Type': 'text/html'
+        });
+        fs.readFile('index.html', null, function (error, data) {
+            if (error) {
+                response.writeHead(404);
+                respone.write('Whoops! File not found!');
+            } else {
+                response.write(data);
+            }
+            response.end();
+        });
+       });
+
+// http.createServer(handleRequest).listen(34010);
 
 /*
     LISTENER
 */
-// app.listen(PORT, function(){            // This is the basic syntax for what is called the 'listener' which receives incoming requests on the specified PORT.
-//     console.log('Express started on http://localhost:' + PORT + '; press Ctrl-C to terminate.')
-// });
+app.listen(PORT, function(){            // This is the basic syntax for what is called the 'listener' which receives incoming requests on the specified PORT.
+    console.log('Express started on http://localhost:' + PORT + '; press Ctrl-C to terminate.')
+});
