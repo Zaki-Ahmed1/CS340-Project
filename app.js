3467627a85a91c22796fe6ff9fce5745e6056789
Zaki Ahmed <ZakiAhmed@alumni.usc.edu>
2021-11-08 04:27:01 -0500

    push

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-// ...UNCOMMENT THE DIFFERENT PORT NUMBERS FOR DEPLOYING AND RUNNING LOCALLY VS. ON OSU...
 // ----------------------------------------------------START---------------------------------------------------------
 //cd into root, Run "npm install", and then run "node app.js"...
 
@@ -12,6 +11,9 @@ app.get('/', function(request, response){
     response.sendFile('index.html', { root: './pages' });
 });
 app.get('/index', function(request, response){
+    response.sendFile('index.html', { root: './pages' });
+});
+app.get('/index.html', function(request, response){
     response.sendFile('index.html', { root: './pages' });
 });
 app.get('/products', function(request, response){
@@ -30,32 +32,9 @@ app.get('/carts', function(request, response){
     response.sendFile('carts.html', { root: './pages' });
 });
 
-// LOCAL INSTANCE
-// app.get('/', function(request, response){
-//     response.sendFile('index.html', { root: './pages-local' });
-// });
-// app.get('/index', function(request, response){
-//     response.sendFile('index.html', { root: './pages-local' });
-// });
-// app.get('/products', function(request, response){
-//     response.sendFile('products.html', { root: './pages-local' });
-// });
-// app.get('/employees', function(request, response){
-//     response.sendFile('employees.html', { root: './pages-local' });
-// });
-// app.get('/customers', function(request, response){
-//     response.sendFile('customers.html', { root: './pages-local' });
-// });
-// app.get('/orders', function(request, response){
-//     response.sendFile('orders.html', { root: './pages-local' });
-// });
-// app.get('/carts', function(request, response){
-//     response.sendFile('carts.html', { root: './pages-local' });
-// });
-
 
 app.listen(PORT, function(){            // This is the basic syntax for what is called the 'listener' which receives incoming requests on the specified PORT.
     console.log('Express started on http://localhost:' + PORT + '; press Ctrl-C to terminate.')
 });
-// Use this in browser - http://localhost:8000/index.html
+// Use this in browser - http://localhost:12626/
 // ----------------------------------------------------END---------------------------------------------------------
