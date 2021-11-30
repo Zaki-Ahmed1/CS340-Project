e1c7b41f6c114343536136664231ce59597e8aca
Zaki Ahmed <ahmedza@flip1.engr.oregonstate.edu>
2021-11-30 00:20:06 -0800

    messin around with DB connectivity

--- app.js
+++ app.js
@@ -4,11 +4,22 @@
 // Express
 const express   = require('express');
 const app       = new express();
+var mysql = require('mysql')
 PORT            = 12626;
+
+var db = require('./db-connector')
 
 // OSU SERVERS
 app.get('/', function(request, response){
     response.sendFile('index.html', { root: './pages' });
+
+    // query1 = 'SELECT * FROM products;'
+    // // SELECT *...
+    // db.pool.query(query1, function(err, results, fields){
+    //     // Send the results to the browser
+    //     let base = "<h1>MySQL Results:</h1>"
+    //     res.send(base + JSON.stringify(results));
+    // });
 });
 app.get('/index', function(request, response){
     response.sendFile('index.html', { root: './pages' });
@@ -36,5 +47,7 @@ app.get('/carts', function(request, response){
 app.listen(PORT, function(){            // This is the basic syntax for what is called the 'listener' which receives incoming requests on the specified PORT.
     console.log('Express started on http://localhost:' + PORT + '; press Ctrl-C to terminate.')
 });
+
+// Use this in browser - http://flip1.engr.oregonstate.edu:12626/
 // Use this in browser - http://localhost:12626/
 // ----------------------------------------------------END---------------------------------------------------------
